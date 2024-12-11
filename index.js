78d12972997b8171548c9c3bfc49c74c43ea7f1c
Anus Younus <AnusYounus254@gmail.com>
2024-12-11 10:42:32 +0500

    class 2 of server type is routing with express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express"
-
+import userRouter from './routes/user.js'
 const app = express()
 const PORT = 4000
+
+app.use(express.json())
+app.use("/user", userRouter)
 
 function middleware(req, res, next) {
     req.accepted = true
